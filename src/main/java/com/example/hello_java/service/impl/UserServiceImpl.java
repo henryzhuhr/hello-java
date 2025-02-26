d04434f4e8585b55566b9d5677a1223289720c87
henryzhuhr <henry.zhuhr@icloud.com>
2025-02-26 23:17:20 +0800

    🐣 feat: 配置文件注入变量

--- src/main/java/com/example/hello_java/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/hello_java/service/impl/UserServiceImpl.java
@@ -8,11 +8,19 @@ import com.example.hello_java.exception.UserNotFoundException;
 import com.example.hello_java.repository.UserRepository;
 import com.example.hello_java.service.UserService;
 
+import org.springframework.beans.factory.annotation.Value;
+
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    /**
+     * 默认用户名 通过 @Value 注解注入配置文件中的属性
+     */
+    @Value("${user.default.username}")
+    private String defaultUsername;
 
     @Override
     public User getUserById(String id) {
@@ -32,6 +40,11 @@ public class UserServiceImpl implements UserService {
         // 会在插入新记录时自动生成一个 UUID 作为主键。这意味着你不需要手动设置 id，Hibernate 会自动处理。
         user.setId(null);
         // TODO: 需要需要处理 uuid 存在的情况
+
+        // 如果用户没有提供 username，则使用默认值
+        if (user.getUsername() == null || user.getUsername().isEmpty()) {
+            user.setUsername(defaultUsername);
+        }
 
         try {
             User insertedUser = userRepository.save(user);
